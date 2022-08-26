44af27c1dfca3a3b6f6db6dc321e74f783efafa1
Lena <summyalena@gmail.com>
2022-08-26 17:03:17 +0100

    done

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
